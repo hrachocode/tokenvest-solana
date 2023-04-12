243446820be83e15bc943c99cdee450978ef77e7
tumero58 <erik.tumasyan@inomma.com>
2023-04-12 14:55:48 +0400

    feat: lint

--- frontend/src/components/TvButton/TvButton.styles.ts
+++ frontend/src/components/TvButton/TvButton.styles.ts
@@ -1,47 +1,47 @@
 const primaryButtonStyles = {
-    minHeight:"72px",
-    background:"#A259FF",
-    borderRadius:"20px",
-    color:"#FFFFFF",
-    padding:"0px 50px 0px 50px",
-    fontWeight:600,
-    fontSize:"22px",
-    border: "2px solid #A259FF",
-    ":hover": {
-        background: "#2B2B2B"
-    }
+  minHeight: "72px",
+  background: "#A259FF",
+  borderRadius: "20px",
+  color: "#FFFFFF",
+  padding: "0px 50px 0px 50px",
+  fontWeight: 600,
+  fontSize: "22px",
+  border: "2px solid #A259FF",
+  ":hover": {
+    background: "#2B2B2B"
+  }
 };
 
 const secondaryButtonStyles = {
-    minHeight:"60px",
-    background:"#A259FF",
-    borderRadius:"20px",
-    color:"#FFFFFF",
-    padding:"0px 50px 0px 50px",
-    fontWeight:600,
-    fontSize:"16px",
-    border: "2px solid #A259FF",
-    ":hover": {
-        background: "#2B2B2B"
-    }
+  minHeight: "60px",
+  background: "#A259FF",
+  borderRadius: "20px",
+  color: "#FFFFFF",
+  padding: "0px 50px 0px 50px",
+  fontWeight: 600,
+  fontSize: "16px",
+  border: "2px solid #A259FF",
+  ":hover": {
+    background: "#2B2B2B"
+  }
 };
 
 const tertiaryButtonStyles = {
-    minHeight:"46px",
-    background:"#A259FF",
-    borderRadius:"20px",
-    color:"#FFFFFF",
-    padding:"0px 50px 0px 50px",
-    fontWeight:600,
-    fontSize:"16px",
-    border: "2px solid #A259FF",
-    ":hover": {
-        background: "#2B2B2B"
-    }
+  minHeight: "46px",
+  background: "#A259FF",
+  borderRadius: "20px",
+  color: "#FFFFFF",
+  padding: "0px 50px 0px 50px",
+  fontWeight: 600,
+  fontSize: "16px",
+  border: "2px solid #A259FF",
+  ":hover": {
+    background: "#2B2B2B"
+  }
 };
 
 export const buttonStyles = {
-    primary: primaryButtonStyles,
-    secondary: secondaryButtonStyles,
-    tertiary: tertiaryButtonStyles,
+  primary: primaryButtonStyles,
+  secondary: secondaryButtonStyles,
+  tertiary: tertiaryButtonStyles,
 };
--- frontend/src/theme/theme.ts
+++ frontend/src/theme/theme.ts
@@ -2,5 +2,5 @@ import { createTheme } from "@mui/material";
 import { typography } from "./typography";
 
 export const theme = createTheme({
-    typography
+  typography
 });
