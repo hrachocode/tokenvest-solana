81f09501ad98e01ddbc74c5c3596f76c1af28fe5
tumero58 <erik.tumasyan@inomma.com>
2023-04-12 14:59:43 +0400

    feat: optimize style for button

--- frontend/src/components/TvButton/TvButton.styles.ts
+++ frontend/src/components/TvButton/TvButton.styles.ts
@@ -1,43 +1,33 @@
-const primaryButtonStyles = {
-  minHeight: "72px",
+const baseButtonStyles = {
   background: "#A259FF",
   borderRadius: "20px",
   color: "#FFFFFF",
   padding: "0px 50px 0px 50px",
-  fontWeight: 600,
-  fontSize: "22px",
   border: "2px solid #A259FF",
   ":hover": {
     background: "#2B2B2B"
   }
+}
+
+const primaryButtonStyles = {
+  ...baseButtonStyles,
+  minHeight: 72,
+  fontWeight: 600,
+  fontSize: 22,
 };
 
 const secondaryButtonStyles = {
-  minHeight: "60px",
-  background: "#A259FF",
-  borderRadius: "20px",
-  color: "#FFFFFF",
-  padding: "0px 50px 0px 50px",
+  ...baseButtonStyles,
+  minHeight: 60,
   fontWeight: 600,
-  fontSize: "16px",
-  border: "2px solid #A259FF",
-  ":hover": {
-    background: "#2B2B2B"
-  }
+  fontSize: 16,
 };
 
 const tertiaryButtonStyles = {
-  minHeight: "46px",
-  background: "#A259FF",
-  borderRadius: "20px",
-  color: "#FFFFFF",
-  padding: "0px 50px 0px 50px",
+  ...baseButtonStyles,
+  minHeight: 46,
   fontWeight: 600,
-  fontSize: "16px",
-  border: "2px solid #A259FF",
-  ":hover": {
-    background: "#2B2B2B"
-  }
+  fontSize: 16,
 };
 
 export const buttonStyles = {
