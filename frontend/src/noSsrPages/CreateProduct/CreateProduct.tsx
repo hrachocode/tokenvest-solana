53979410eb878e1e0b4167cb13dbc402656f62e5
tumero58 <erik.tumasyan@inomma.com>
2023-04-13 14:45:55 +0400

    feat: add input validation utility function

--- frontend/src/noSsrPages/CreateProduct/CreateProduct.tsx
+++ frontend/src/noSsrPages/CreateProduct/CreateProduct.tsx
@@ -5,6 +5,7 @@ import { usePolkadot } from "@/hooks/usePolkadot";
 import { Box, Typography } from "@mui/material";
 import { ChangeEvent, useState } from "react";
 import { styles } from "./CreateProduct.styles";
+import { inputValidator } from "./utils";
 
 const CreateProduct = (): JSX.Element => {
     const [name, setName] = useState("");
@@ -16,8 +17,14 @@ const CreateProduct = (): JSX.Element => {
         func(e.target.value);
     };
 
-    const handleClick = () => {
-        deploy(SHIBUYA_ADDRESS, name, raiseGoal, sharePercentage);
+    const handleClick = async () => {
+        const isValid = inputValidator(sharePercentage);
+        if (isValid.success) {
+            await deploy(SHIBUYA_ADDRESS, name, raiseGoal, sharePercentage);
+        } else {
+            alert(isValid.message);
+        }
+
     };
 
     return (
@@ -28,11 +35,11 @@ const CreateProduct = (): JSX.Element => {
             </Box>
             <Box>
                 <Typography>Raise Goal</Typography>
-                <TvInput customVariant="tertiary" onChange={(e) => { handleChange(e, setRaiseGoal) }} />
+                <TvInput type="number" customVariant="tertiary" onChange={(e) => { handleChange(e, setRaiseGoal) }} />
             </Box>
             <Box>
                 <Typography>Share percentage</Typography>
-                <TvInput customVariant="tertiary" onChange={(e) => { handleChange(e, setSharePercentage) }} />
+                <TvInput type="number" customVariant="tertiary" onChange={(e) => { handleChange(e, setSharePercentage) }} />
             </Box>
             <Box>
                 <TvButton onClick={handleClick}>Create Product</TvButton>
