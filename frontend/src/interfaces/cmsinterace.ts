7b605a863e35f756a32a9d7db87513962b9410be
tumero58 <erik.tumasyan@inomma.com>
2023-04-19 16:42:47 +0400

    feat: create products page

---
+++ frontend/src/interfaces/cmsinterace.ts
@@ -0,0 +1,30 @@
+export interface ICMSProduct {
+    id: string;
+    attributes: {
+        title: string;
+        raiseGoal: string;
+        sharePercentage: string;
+        address: string;
+        ownerAddress: string;
+        ownerName: string;
+        raisedAmount: string;
+        image: {
+            data: {
+                attributes: {
+                    url: string
+                }
+            }
+        };
+    }
+}
+export interface IProduct {
+    id: string;
+    title: string;
+    raiseGoal: string;
+    sharePercentage: string;
+    address: string;
+    ownerAddress: string;
+    ownerName: string;
+    raisedAmount: string;
+    image: string | null;
+}
--- frontend/src/pages/products/index.tsx
+++ frontend/src/pages/products/index.tsx
@@ -1,9 +1,42 @@
-import { Box, Typography } from "@mui/material";
+import { TvProduct } from "@/components/TvProduct/TvProduct";
+import { CMS_API, CMS_PRODUCTS, POPULATE_ALL } from "@/constants/cms";
+import { ICMSProduct, IProduct } from "@/interfaces/cmsinterace";
+import { Box } from "@mui/material";
+import { Fragment } from "react";
 
-const Products = () => {
+export async function getStaticProps() {
+  const res = await fetch(`${CMS_API}${CMS_PRODUCTS}${POPULATE_ALL}`);
+  const data = await res.json();
+
+  const products: IProduct[] = data?.data?.map((item: ICMSProduct) => {
+    return {
+      id: item.id,
+      title: item.attributes.title,
+      raiseGoal: item.attributes.raiseGoal,
+      sharePercentage: item.attributes.sharePercentage,
+      address: item.attributes.address,
+      ownerAddress: item.attributes.ownerAddress,
+      ownerName: item.attributes.ownerName,
+      raisedAmount: item.attributes.raisedAmount,
+      image: item.attributes.image?.data?.attributes?.url || null
+    };
+  }) || [];
+
+  return {
+    props: {
+      products
+    }
+  };
+}
+
+const Products = ({ products }: { products: IProduct[] }) => {
   return (
     <Box>
-      <Typography>Products</Typography>
+      {products.length !== 0 ? products?.map((item, index) =>
+        <Fragment key={index + 1}>
+          <TvProduct product={item} />
+        </Fragment>
+      ) : <></>}
     </Box>
   );
 };
