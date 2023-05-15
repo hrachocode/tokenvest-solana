73f12e800c021a77ef6a9f68b80613f29d63f2e8
tumero58 <erik.tumasyan@inomma.com>
2023-05-15 14:46:57 +0400

    feat: create footer component

---
+++ frontend/src/constants/footer.ts
@@ -0,0 +1,14 @@
+export const FOOTER_MARKETPLACE = {
+  TITLE: "Products Marketplace",
+  TEXT1: "Products marketplace UI created with Anima for Figma.",
+  TEXT2: "Join our community"
+};
+
+export const FOOTER_EXPLORE = {
+  TITLE: "Explore",
+  MARKETPLACE: "Marketplace",
+  RANKINGS: "Rankings",
+  WALLET: "Connect a wallet"
+};
+
+export const COPYRIGHT_TEXT = "Ⓒ Tokenvest Market.";
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Box, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
 import { theme } from "../theme/theme";
 import { appStyles } from "@/styles/App.styles";
+import { Footer } from "@/components/Footer/Footer";
 
 const Header = dynamic(() => import("../components/Header/Header"), {
   ssr: false
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Box sx={appStyles.component}>
         <Component {...pageProps} />
       </Box>
+      <Footer />
     </ThemeProvider>
   );
 }
