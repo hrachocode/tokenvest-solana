2e47293a6bcb873c043a792935d82a0c8930c075
tumero58 <erik.tumasyan@inomma.com>
2023-05-26 14:55:12 +0400

    feat: initial commit

--- frontend/src/components/CreateProduct/CreateProduct.tsx
+++ frontend/src/components/CreateProduct/CreateProduct.tsx
@@ -1,21 +1,58 @@
 import { TvButton } from "@/components/TvButton/TvButton";
 import { TvInput } from "@/components/TvInput/TvInput";
 import { TvSelect } from "@/components/TvSelect/TvSelect";
+import { CMS_API, CMS_LAST_CHANGES, EQUALS, FILTERS, OWNER_ADDRESS, POPULATE_ALL } from "@/constants/cms";
 import { LABEL_CATEGORY, LABEL_DAYS } from "@/constants/general";
 import { SHIBUYA_ACCOUNT_NAME, SHIBUYA_ADDRESS } from "@/constants/polkadot";
 import { selectOptions } from "@/constants/selectOptions";
 import { ICategory } from "@/interfaces/cmsinterace";
 import { createProductCMS } from "@/utils/cmsUtils";
-import { Box, SelectChangeEvent, Typography } from "@mui/material";
-import { useState } from "react";
+import { handleRequest, METHODS } from "@/utils/handleRequest";
+import { Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import { styles } from "./CreateProduct.styles";
-import { inputValidator } from "./utils";
+import { inputValidator, handleBlur, handleChange, handleFileChange, handleChangeSelect } from "./utils";
 
 interface ICreateProduct {
   categories: ICategory[];
 }
 
 const CreateProduct = ({ categories }: ICreateProduct): JSX.Element => {
+
+  useEffect(() => {
+    (async () => {
+      const { data = [] } = await handleRequest(
+        `${CMS_API}${CMS_LAST_CHANGES}${POPULATE_ALL}&${FILTERS}[${OWNER_ADDRESS}][${EQUALS}]=${SHIBUYA_ADDRESS}`
+        , METHODS.GET) ?? {};
+      if (data.length !== 0) {
+        setName(data[0].attributes.title);
+        setDescription(data[0].attributes.description);
+        setRaiseGoal(data[0].attributes.raiseGoal);
+        setSharePercentage(data[0].attributes.sharePercentage);
+        setDays(data[0].attributes.days);
+        setCategory(data[0].attributes.category.data.id);
+        setEditId(data[0].id);
+      }
+
+      // if (data.length === 0) {
+      //   const postRes = await handleRequest(`${CMS_API}${CMS_LAST_CHANGES}`, METHODS.POST, {
+      //     data: {
+      //       title: "",
+      //       description: "",
+      //       raiseGoal: 0,
+      //       sharePercentage: 0,
+      //       ownerAddress: SHIBUYA_ADDRESS,
+      //       ownerName: "",
+      //       raisedAmount: 0,
+      //       days: 0,
+      //       category: 1
+      //     }
+      //   });
+      //   const { data = [] } = await handleRequest(`${CMS_API}${CMS_LAST_CHANGES}${POPULATE_ALL}`, METHODS.GET) ?? {};
+      //   console.log(data, "DATAAA ????");
+      // }
+    })();
+  }, []);
 
   const selectCategories = categories.map((item: ICategory) => {
     return {
@@ -31,22 +68,7 @@ const CreateProduct = ({ categories }: ICreateProduct): JSX.Element => {
   const [ files, setFiles ] = useState([]);
   const [ days, setDays ] = useState("");
   const [ category, setCategory ] = useState("");
-
-  const handleChange = (value: string, cb: Function) => {
-    cb(value);
-  };
-
-  const handleFileChange = ({ target: { files = [] } = {} }: any) => {
-    setFiles(files);
-  };
-
-  const handleChangeDays = (event: SelectChangeEvent) => {
-    setDays(event.target.value as string);
-  };
-
-  const handleChangeCategory = (event: SelectChangeEvent) => {
-    setCategory(event.target.value as string);
-  };
+  const [ editId, setEditId ] = useState(0);
 
   const handleClick = async () => {
     const { success = false, message = "" } = inputValidator(sharePercentage) ?? {};
@@ -62,31 +84,57 @@ const CreateProduct = ({ categories }: ICreateProduct): JSX.Element => {
         category);
     } else {
       alert(message);
-    }
+    };
 
   };
   return (
     <Box sx={styles.createProductWrapper}>
       <Box>
         <Typography>Name</Typography>
-        <TvInput customVariant="tertiary" onChange={({ target: { value = "" } = {} }) => { handleChange(value, setName); }} />
+        <TvInput
+          value={name}
+          customVariant="tertiary"
+          onChange={({ target: { value = "" } = {} }) => { handleChange(value, setName); }}
+          onBlur={({ target: { value = "" } = {} }) => { handleBlur(value, "title", editId); }}
+        />
       </Box>
       <Box>
         <Typography>Description</Typography>
-        <TvInput customVariant="tertiary" onChange={({ target: { value = "" } = {} }) => { handleChange(value, setDescription); }} />
+        <TvInput
+          value={description}
+          customVariant="tertiary"
+          onChange={({ target: { value = "" } = {} }) => { handleChange(value, setDescription); }}
+          onBlur={({ target: { value = "" } = {} }) => { handleBlur(value, "description", editId); }}
+        />
       </Box>
       <Box>
         <Typography>Raise Goal</Typography>
-        <TvInput type="number" customVariant="tertiary" onChange={({ target: { value = "" } = {} }) => { handleChange(value, setRaiseGoal); }} />
+        <TvInput
+          value={raiseGoal}
+          type="number"
+          customVariant="tertiary"
+          onChange={({ target: { value = "" } = {} }) => { handleChange(value, setRaiseGoal); }}
+          onBlur={({ target: { value = "" } = {} }) => { handleBlur(value, "raiseGoal", editId); }}
+        />
       </Box>
       <Box>
         <Typography>Share percentage</Typography>
-        <TvInput type="number" customVariant="tertiary" onChange={({ target: { value = "" } = {} }) => { handleChange(value, setSharePercentage); }} />
+        <TvInput
+          value={sharePercentage}
+          type="number"
+          customVariant="tertiary"
+          onChange={({ target: { value = "" } = {} }) => { handleChange(value, setSharePercentage); }}
+          onBlur={({ target: { value = "" } = {} }) => { handleBlur(value, "sharePercentage", editId); }}
+        />
       </Box>
       <Box>
         <Typography>Days</Typography>
         <Box sx={styles.selectWrapper}>
-          <TvSelect label={LABEL_DAYS} value={days} handleChange={handleChangeDays} selectOptions={selectOptions} />
+          <TvSelect
+            label={LABEL_DAYS}
+            value={days}
+            handleChange={(e) => { handleChangeSelect(e, setDays, "days", editId); }}
+            selectOptions={selectOptions} />
         </Box>
       </Box>
       <Box>
@@ -95,13 +143,13 @@ const CreateProduct = ({ categories }: ICreateProduct): JSX.Element => {
           <TvSelect
             label={LABEL_CATEGORY}
             value={category}
-            handleChange={handleChangeCategory}
+            handleChange={(e) => { handleChangeSelect(e, setCategory, "category", editId); }}
             selectOptions={selectCategories} />
         </Box>
       </Box>
       <Box>
         <Typography>Image</Typography>
-        <TvInput type="file" customVariant="tertiary" onChange={handleFileChange} />
+        <TvInput type="file" customVariant="tertiary" onChange={(e) => handleFileChange(e, setFiles)} />
       </Box>
       <Box>
         <TvButton onClick={handleClick}>Create Product</TvButton>
--- frontend/src/components/CreateProduct/utils.ts
+++ frontend/src/components/CreateProduct/utils.ts
@@ -1,5 +1,8 @@
+import { CMS_API, CMS_LAST_CHANGES } from "@/constants/cms";
 import { MAX_SHARE_PERCENTAGE, MIN_SHARE_PERCENTAGE } from "@/constants/contracts";
 import { INVALID_SHARE_PERCENTAGE_MESSAGE } from "@/constants/messages";
+import { handleRequest, METHODS } from "@/utils/handleRequest";
+import { SelectChangeEvent } from "@mui/material";
 
 export const inputValidator = (sharePercentage: string) => {
   if (Number(sharePercentage) > MAX_SHARE_PERCENTAGE || Number(sharePercentage) < MIN_SHARE_PERCENTAGE) {
@@ -12,4 +15,29 @@ export const inputValidator = (sharePercentage: string) => {
       success: true
     };
   }
+};
+
+export const handleBlur = async (value: string, fieldKey: string, editId: number) => {
+  try {
+    await handleRequest(`${CMS_API}${CMS_LAST_CHANGES}/${editId}`, METHODS.PUT, {
+      data: {
+        [fieldKey]: value,
+      }
+    });
+  } catch (error) {
+    alert(error);
+  }
+};
+
+export const handleChange = (value: string, cb: Function) => {
+  cb(value);
+};
+
+export const handleFileChange = ({ target: { files = [] } = {} }: any, cb: Function) => {
+  cb(files);
+};
+
+export const handleChangeSelect = (event: SelectChangeEvent, cb: Function, fieldKey: string, editId: number) => {
+  cb(event.target.value as string);
+  handleBlur(event.target.value, fieldKey, editId);
 };
