0ca77f15730e44627a8a0764d34076c8416047ba
DavidSimonyan95 <davo.simonyan1195@gmail.com>
2023-10-11 16:51:22 +0400

    add favivon and title

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [ "localhost", "tokenvest.net" ],
+    domains: ["localhost", "tokenvest.net", "tokenvest-solana.vercel.app"],
   },
 };
 
