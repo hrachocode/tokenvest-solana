4fdd7ad3a7402e6f330acf6b7df45e7ae01e8c66
hrachocode <hracho94@yahoo.com>
2023-10-20 00:46:10 +0400

    fixed server js

--- strapi/config/env/production/server.js
+++ strapi/config/env/production/server.js
@@ -1,10 +1,6 @@
 module.exports = ({ env }) => ({
     url: env("RENDER_EXTERNAL_URL"),
     dirs: {
-<<<<<<< HEAD
         public: "/strapi/public"
-=======
-        public: "/public"
->>>>>>> 1b545f5a83f7676c168838ae63585f0cad54fd78
     },
 });
