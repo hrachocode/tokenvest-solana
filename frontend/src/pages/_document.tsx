8af2deb37d299cc6339da89a4f09a77c2970d20c
DavidSimonyan95 <davo.simonyan1195@gmail.com>
2023-11-07 16:25:17 +0400

    have changed script

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -7,25 +7,32 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         {/* Google tag (gtag.js) */}
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-YC927WDBEG"></Script>
-        <Script id="ga-script">
-          {`
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-YC927WDBEG" />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-        
             gtag('config', 'G-YC927WDBEG');
-          `}
-        </Script>
-        <Script id="google-analytics" type="text/javascript">
-          {`
-           (function(c,l,a,r,i,t,y){
-            c[a]=c[a]function(){(c[a].q=c[a].q[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          `,
+          }}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a] = c[a] || function(){(c[a].q = c[a].q || []).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "jme77rm12a");
-          `}
-        </Script>
+          `,
+          }}
+        />
       </Head>
       <body>
         <Main />
