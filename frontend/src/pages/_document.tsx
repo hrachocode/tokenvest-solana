74a947019dd31316f7e830c4e437f639536856ea
DavidSimonyan95 <davo.simonyan1195@gmail.com>
2023-11-07 11:13:28 +0400

    have added scripts

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,10 +1,31 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        {/* Google tag (gtag.js) */}
+        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
+        <Script id="ga-script">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `}
+        </Script>
+        <Script id="google-analytics" type="text/javascript">
+          {`
+           (function(c,l,a,r,i,t,y){
+            c[a]=c[a]function(){(c[a].q=c[a].q[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "jme77rm12a");
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
